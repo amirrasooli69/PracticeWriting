4a3522474993b11ad90992ce44d3b3ffc0d398b5
amirrasooli69 <amirrasooli69@gmail.com>
2020-07-13 22:14:18 +0430

    correct link insta and telegram and wgatsapp

--- app/src/main/java/ir/papiloo/practicewriting/Idea.java
+++ app/src/main/java/ir/papiloo/practicewriting/Idea.java
@@ -3,17 +3,30 @@ package ir.papiloo.practicewriting;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 import ir.papiloo.practicewriting.R;
 
 public class Idea extends AppCompatActivity {
-    BottomNavigationView bottomNav;
+    BottomNavigationView buttomNav;
+    Button btnWhatsApp,btnInsta,btnTelegram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,8 @@ public class Idea extends AppCompatActivity {
         getSupportActionBar().hide();
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        buttomNav = findViewById(R.id.bottom_navigation);
+        buttomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem Item) {
                 if (Item.getItemId() == R.id.nav_home) {
@@ -39,6 +52,51 @@ public class Idea extends AppCompatActivity {
                     return true;
                 }
                 return false;
+            }
+        });
+        btnInsta=findViewById(R.id.btnInstagram);
+        btnInsta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.instagram.com/papiloosoft/");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+                Log.i("Instagram",likeIng.toString());
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/papiloosoft/")));
+                }
+                Toast.makeText(Idea.this, "Instagram", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnWhatsApp=findViewById(R.id.btnWhatsApp);
+        btnWhatsApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
+                    startActivity(launchIntent);
+                }
+                catch (Exception  e){
+                    Toast.makeText(Idea.this, "شما برنامه WhatsApp را ندارید", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnTelegram=findViewById(R.id.btnTelegram);
+        btnTelegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=Papiloosoft"));
+                    startActivity(intent);
+                }
+                catch (ActivityNotFoundException e){
+                Toast.makeText(Idea.this, "شما برنامه Telegram را ندارید", Toast.LENGTH_SHORT).show();
+            }
             }
         });
     }
